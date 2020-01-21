dee2119cc07ec8d5214465c142f28e17030046ab
JSusca <jsusca25@gmail.com>
2020-01-20 23:46:14 -0500

    Add input field to accept alarm time and scrub with regex

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import random
 import webbrowser
+import re
+
+# Play video in new web browser tab
+# webbrowser.open_new_tab(video)
 
 running = True
 
@@ -7,7 +11,9 @@ while running:
     # Choose random video from videos.txt file
     video = random.choice(open("videos.txt").read().split())
 
-    # Play video in new web browser tab
-    webbrowser.open_new_tab(video)
+    alarm = input("Please enter alarm in the following format: 13:30 hour:minute")
+    alarm = re.sub(r'[^:\d]+', '', alarm)
 
-    break
+    print(alarm)
+
+    # Alarm time in sec - current time in sec = sec to sleep
