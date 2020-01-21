b45c7b2fcc6dd4b398e2eaaa93e57e3b02972553
JSusca <jsusca25@gmail.com>
2020-01-20 21:24:14 -0500

    Add comments

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import webbrowser
 running = True
 
 while running:
+    # Choose random video from videos.txt file
     video = random.choice(open("videos.txt").read().split())
 
+    # Play video in new web browser tab
     webbrowser.open_new_tab(video)
 
     break
