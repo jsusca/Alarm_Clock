40730ec724a579b8a3c8e0f93dd75b16bbfc455c
JSusca <jsusca25@gmail.com>
2020-01-25 11:39:23 -0500

    Convert alarm/ current time into seconds. Subtract alarm time from current time to determine sleep_time. Suspend execution for sleep_time. Play random video from videos.txt.

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import time
 import webbrowser
 import re
 
-# Play video in new web browser tab
-# webbrowser.open_new_tab(video)
 
 running = True
 
@@ -12,11 +10,34 @@ while running:
     # Choose random video from videos.txt file
     video = random.choice(open("videos.txt").read().split())
 
+    # Take user input and scrub
     alarm = input("Please enter alarm in the following format: 13:30 hour:minute")
     alarm = re.sub(r'[^:\d]+', '', alarm)
+
+    # Split 24h time into hours and minutes
     alarm = alarm.split(':')
-    print(alarm)
+
+    # Convert hours and minutes into seconds (based on 24hours)
+    alarm_seconds = (int(alarm[0]) * 60 * 60) + (int(alarm[1]) * 60)
+
+    # Set current_time equal to local time
+    current_time = time.strftime('%X', time.localtime())
+
+    # Split current_time into hours and minutes
+    current_time = current_time.split(':')
+
+    # Convert current_time into seconds (based on 24 hours)
+    current_time_seconds_day = (int(current_time[0]) * 60 * 60) + (int(current_time[1]) * 60) + int(current_time[2])
 
     # Alarm time in sec - current time in sec = sec to sleep
+    sleep_time = alarm_seconds - current_time_seconds_day
+    print(sleep_time)
+    print(time.gmtime(sleep_time))
+
+    # Suspend execution for sleep_time
+    time.sleep(sleep_time)
+
+    # Play video in new web browser tab
+    webbrowser.open_new_tab(video)
 
 
